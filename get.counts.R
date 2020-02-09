ffdaa98a4fac035891b1341a48837e84d5bad279
drramki <rr589@drexel.edu>
2020-02-09 14:56:31 -0500

    Delete get.counts.R

--- get.counts.R
+++
@@ -1,8 +0,0 @@
-library(ExomeDepth)
-library(clustermq)
-library(EDM)
-nSamples = NROW(manifest);
-
-foo = input.yaml
-
- Q(get.bam.counts.mosdepth,pkgs="EDM",task_id=1:nSamples,n_jobs=nSamples, max_calls_worker = 1, template=list(job_name="compute_coverage"), export = list(input = foo))
