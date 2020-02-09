39c7abc607426afe04c7f22d801ac65fbd750ac5
drramki <rr589@drexel.edu>
2020-02-09 14:56:24 -0500

    Delete gather.counts.R

--- gather.counts.R
+++
@@ -1,6 +0,0 @@
-library(ExomeDepth)
-library(clustermq)
-library(EDM)
-
-foo = input.yaml
-Q(gather.mosdepth.coverage,pkgs="EDM",task_id=1,n_jobs=1, max_calls_worker = 1,template=list(job_name="gather_coverage"), export = list(input = foo))
